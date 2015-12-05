937bd8df28658bedddc82c200efb63f981521184
Shawn Yang <vvctiger@hotmail.com>
2015-12-05 12:05:04 -0500

    Setting up database connection

---
+++ vvctiger.com_Core/Models/WordPair.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace vvctiger.com_Core
+{
+    public partial class WordPair
+    {
+
+    }
+}
---
+++ vvctiger.com_Core/Repository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace vvctiger.com_Core
+{
+    public class Repository
+    {
+        private vvctigerContext context = new vvctigerContext();
+
+        public void CreateWordPair(WordPair pair)
+        {
+            context.WordPairs.Add(pair);
+            context.SaveChanges();
+        }
+
+        public void UpdateWordPair(WordPair pair)
+        {
+            context.SaveChanges();
+        }
+
+
+    }
+}
